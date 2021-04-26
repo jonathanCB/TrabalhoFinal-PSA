465a6b381d51629a3bdfdd79684b0f2fffdb1df5
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-04-26 09:43:05 -0300

    Primeiro commit, criação das entidades

---
+++ Entities/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Entities.Models.StatusProduto;
+
+namespace Entities.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+        public Status Estado { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public DateTime? DataVenda { get; set; }
+        public int Vendedor { get; set; }
+        public int CategoriaID { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
