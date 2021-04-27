ccfc013bdf7c4e0ddfd20d249459e945435da090
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-04-27 09:28:54 -0300

    Testando consultas no Program.cs e Consulta 4 corrigida

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,6 +1,10 @@
 ﻿using BLL;
 using Entities.Models;
+using Entities.ViewModels;
+using PL;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleTests
 {
@@ -8,33 +12,163 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            // apenas para testes
-            Facade _adm = new Facade();
+            SecondHandContext context = new SecondHandContext();
 
-            _adm.Add(new Grupo()
+            #region testando conteudo de cada "tabela"
+            List<Produto> prod = context.Produtos.ToList();
+            List<Categoria> cate = context.Categorias.ToList();
+
+            foreach (Produto p in prod)
             {
-                Nome = "Grupo campeao",
-                Integrantes = "<matr1, nome1> , <matr2,nome2>,...",
-                GitHub = "http://github.com/meuprojetoPrivado",
-                Comentarios = "Teste de insercao"
-            });
+                Console.WriteLine("{0}  {1}  {2}  {3}  {4}  {5}  {6}",
+                                    p.ProdutoId, p.Name, p.Descricao, p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
+            }
+            Console.WriteLine("\n\n");
 
-            _adm.Add(new Grupo()
+            foreach (Categoria c in cate)
             {
-                Nome = "Grupo dois",
-                Integrantes = "<matr2.1, nome2.1> , <matr2.2,nome2.2>",
-                GitHub = "http://github.com/projeto2Privado",
-                Comentarios = "So para teste nao faz sentido dois grupos"
-            });
+                Console.WriteLine("{0}  {1}",
+                                    c.CategoriaId, c.Name);
+            }
+            Console.WriteLine("\n\n");
+            #endregion
 
-            foreach (Grupo g in _adm.GetGrupos())
+            #region testando consultas da entrega 1            
+
+            Console.WriteLine("1 - Itens a venda de uma determinada categoria");
+            int cat = 1;
+
+            var consulta1 = context.Produtos
+                            .Where(p => p.Estado == StatusProduto.Status.Disponivel)
+                            .Where(p => p.CategoriaID == cat)
+                            .Select(p => new
+                            {
+                                p.ProdutoId,
+                                p.Name,
+                                p.Descricao,
+                                p.Estado,
+                                p.Valor,
+                                p.Vendedor,
+                                p.CategoriaID
+                            });
+
+            foreach (var p in consulta1)
             {
-                Console.WriteLine("Nome: {0}\n\tid: {1}\n\tComponentes: {2}\n\tComentarios: {3}\n",
-                    g.Nome, g.GrupoId, g.Integrantes, g.Comentarios);
-
+                Console.WriteLine("ProdutoId:{0}  Name:{1}  Descricao:{2}  Estado:{3}  Valor:{4}  Vendedor:{5}  CategoriaID:{6}",
+                                    p.ProdutoId, p.Name, p.Descricao,
+                                    p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
             }
 
 
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("2 - Itens a venda dada uma palavra chave e uma categoria");
+            cat = 9;
+            String palChave = "yamaha";
+
+            var consulta2 = context.Produtos
+                            .Where(p => p.Estado == StatusProduto.Status.Disponivel)
+                            .Where(p => p.CategoriaID == cat)
+                            .Where(p => p.Name.ToUpper().Contains(palChave.ToUpper()) || p.Descricao.ToUpper().Contains(palChave.ToUpper()))
+                            .Select(p => new
+                            {
+                                p.ProdutoId,
+                                p.Name,
+                                p.Descricao,
+                                p.Estado,
+                                p.Valor,
+                                p.Vendedor,
+                                p.CategoriaID
+                            });
+
+
+            foreach (var p in consulta2)
+            {
+                Console.WriteLine("ProdutoId:{0}  Name:{1}  Descricao:{2}  Estado:{3}  Valor:{4}  Vendedor:{5}  CategoriaID:{6}",
+                                    p.ProdutoId, p.Name, p.Descricao,
+                                    p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
+            }
+
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("3 - Itens a venda dentro de uma faixa de valores");
+            decimal valIni = 290.0m;
+            decimal valFin = 500.0m;
+
+            var consulta3 = context.Produtos
+                            .Where(p => p.Estado == StatusProduto.Status.Disponivel)
+                            .Where(p => p.Valor >= valIni && p.Valor <= valFin)
+                            .Select(p => new
+                            {
+                                p.ProdutoId,
+                                p.Name,
+                                p.Descricao,
+                                p.Estado,
+                                p.Valor,
+                                p.Vendedor,
+                                p.CategoriaID
+                            });
+
+            foreach (var p in consulta3)
+            {
+                Console.WriteLine("ProdutoId:{0}  Name:{1}  Descricao:{2}  Estado:{3}  Valor:{4}  Vendedor:{5}  CategoriaID:{6}",
+                                    p.ProdutoId, p.Name, p.Descricao,
+                                    p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
+            }
+
+
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("4 - Itens anunciados por um determinado vendedor, agrupados pelo status da venda");
+            int vend = 1;
+
+            var consulta4 = (from p in context.Produtos
+                            where p.Vendedor == vend
+                            select new
+                            {
+                                p.Name,
+                                p.Estado
+                            }).OrderByDescending(e => e.Estado);
+
+            foreach (var result in consulta4)
+            {
+                Console.WriteLine("VendedorID: {0}\nProduto: {1}\nEstado: {2}\n", vend, result.Name, result.Estado);
+            }
+
+
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("número total de itens vendidos num período e o valor total destas vendas");
+            DateTime dtIni = new DateTime(2020, 04, 01, 00, 00, 00);
+            DateTime dtFin = new DateTime(2020, 05, 01, 00, 00, 00);
+
+            var consulta5 = from p in context.Produtos
+                            where p.Estado == StatusProduto.Status.Vendido
+                            where p.DataVenda >= dtIni && p.DataVenda <= dtFin
+                            select p.Valor;
+
+            var consulta5_1 = from p in consulta5
+                              group p by 1 into grp
+                              select new
+                              {
+                                  quanti = grp.Count(),
+                                  total = grp.Sum()
+                              };
+
+
+            foreach (var p in consulta5_1)
+            {
+                Console.WriteLine("quantidade de produtos vendidos:{0}    valor total:{1}",
+                                    p.quanti, p.total);
+            }
+
+            Console.WriteLine("\n\n");
+
+            #endregion
         }
     }
 }
