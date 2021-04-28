a939be89fb5ad6e22526db9d48ddc1e5b4c6b811
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-04-28 20:42:54 -0300

    Salvando infos antes do merge

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -2,6 +2,7 @@
 using Entities.Models;
 using Entities.ViewModels;
 using PL;
+using PL.DAO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,10 @@ namespace ConsoleTests
             #region testando conteudo de cada "tabela"
             List<Produto> prod = context.Produtos.ToList();
             List<Categoria> cate = context.Categorias.ToList();
+
+            /*ProdutoEF cons = new ProdutoEF();
+            Console.WriteLine("Consulta por categoria: " + cons.ItenCategorias("Celular").ToString());*/
+            
 
             foreach (Produto p in prod)
             {
@@ -35,59 +40,52 @@ namespace ConsoleTests
 
             #region testando consultas da entrega 1            
 
-            Console.WriteLine("1 - Itens a venda de uma determinada categoria");
-            int cat = 1;
+            Console.WriteLine("1 - Itens a venda de uma determinada categoria = Celular");
+            String cat = "Celular";
 
             var consulta1 = context.Produtos
                             .Where(p => p.Estado == StatusProduto.Status.Disponivel)
-                            .Where(p => p.CategoriaID.CategoriaId == cat)
+                            .Where(p => p.Categoria.ToUpper() == cat.ToUpper())
                             .Select(p => new
                             {
-                                p.ProdutoId,
                                 p.Name,
                                 p.Descricao,
-                                p.Estado,
                                 p.Valor,
                                 p.Vendedor,
-                                p.CategoriaID
+                                p.Categoria
                             });
 
             foreach (var p in consulta1)
             {
-                Console.WriteLine("ProdutoId:{0}  Name:{1}  Descricao:{2}  Estado:{3}  Valor:{4}  Vendedor:{5}  CategoriaID:{6}",
-                                    p.ProdutoId, p.Name, p.Descricao,
-                                    p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
+                Console.WriteLine("Name:{0}  Descricao:{1}  Valor:{2}  Vendedor:{3}  Categoria:{4}",
+                                    p.Name, p.Descricao, p.Valor, p.Vendedor, p.Categoria);
             }
-
 
             Console.WriteLine("\n\n");
 
 
             Console.WriteLine("2 - Itens a venda dada uma palavra chave e uma categoria");
-            cat = 9;
+            cat = "Instrumentos Musicais";
             String palChave = "yamaha";
 
             var consulta2 = context.Produtos
                             .Where(p => p.Estado == StatusProduto.Status.Disponivel)
-                            .Where(p => p.CategoriaID.CategoriaId == cat)
+                            .Where(p => p.Categoria.ToUpper() == cat.ToUpper())
                             .Where(p => p.Name.ToUpper().Contains(palChave.ToUpper()) || p.Descricao.ToUpper().Contains(palChave.ToUpper()))
                             .Select(p => new
                             {
-                                p.ProdutoId,
                                 p.Name,
                                 p.Descricao,
-                                p.Estado,
                                 p.Valor,
                                 p.Vendedor,
-                                p.CategoriaID
+                                p.Categoria
                             });
 
 
             foreach (var p in consulta2)
             {
-                Console.WriteLine("ProdutoId:{0}  Name:{1}  Descricao:{2}  Estado:{3}  Valor:{4}  Vendedor:{5}  CategoriaID:{6}",
-                                    p.ProdutoId, p.Name, p.Descricao,
-                                    p.Estado, p.Valor, p.Vendedor, p.CategoriaID);
+                Console.WriteLine("Name:{0}  Descricao:{1}  Valor:{2}  Vendedor:{3}  Categoria:{4}",
+                                    p.Name, p.Descricao, p.Valor, p.Vendedor, p.Categoria);
             }
 
             Console.WriteLine("\n\n");
@@ -135,7 +133,7 @@ namespace ConsoleTests
 
             foreach (var result in consulta4)
             {
-                Console.WriteLine("VendedorID: {0}\nProduto: {1}\nEstado: {2}\n", vend, result.Name, result.Estado);
+                Console.WriteLine("VendedorID: {0}\nProduto: {1}\nStatus: {2}\n", vend, result.Name, result.Estado);
             }
 
 
