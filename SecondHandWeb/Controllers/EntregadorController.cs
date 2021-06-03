e05279f142867c92d70d45fedcbf1c620b115323
Miguel Zanela <miguelzanela89@gmail.com>
2021-06-02 21:35:48 -0300

    entregador criado, tirado edit e delete

---
+++ SecondHandWeb/Controllers/EntregadorController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+using PL.Context;
+
+namespace SecondHandWeb.Controllers
+{
+    public class EntregadorController : Controller
+    {
+        private readonly SecondHandContext _context;
+
+        public EntregadorController(SecondHandContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entregador
+        public async Task<IActionResult> Index()
+        {
+            var secondHandContext = _context.Produtos.Include(p => p.Categoria);
+            return View(await secondHandContext.ToListAsync());
+        }
+
+        // GET: Entregador/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // GET: Entregador/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaID"] = new SelectList(_context.Categorias, "CategoriaId", "Name");
+            return View();
+        }
+
+        // POST: Entregador/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProdutoId,Name,Descricao,Estado,Valor,DataEntrada,DataVenda,UsuarioIDVendedor,NomeVendedor,UsuarioIDComprador,NomeComprador,CategoriaID")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(produto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaID"] = new SelectList(_context.Categorias, "CategoriaId", "Name", produto.CategoriaID);
+            return View(produto);
+        }
+
+        // GET: Entregador/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaID"] = new SelectList(_context.Categorias, "CategoriaId", "Name", produto.CategoriaID);
+            return View(produto);
+        }
+
+        // POST: Entregador/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ProdutoId,Name,Descricao,Estado,Valor,DataEntrada,DataVenda,UsuarioIDVendedor,NomeVendedor,UsuarioIDComprador,NomeComprador,CategoriaID")] Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExists(produto.ProdutoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaID"] = new SelectList(_context.Categorias, "CategoriaId", "Name", produto.CategoriaID);
+            return View(produto);
+        }
+
+        // GET: Entregador/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Entregador/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProdutoExists(long id)
+        {
+            return _context.Produtos.Any(e => e.ProdutoId == id);
+        }
+    }
+}
